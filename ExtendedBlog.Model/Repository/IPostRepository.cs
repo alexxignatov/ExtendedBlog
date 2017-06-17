73daf4676637ea3f7dc5b56c46a3ee671e12cca1
Alex <primat1987@users.noreply.github.com>
2017-06-18 00:52:16 +0300

    Update IPostRepository.cs

--- ExtendedBlog.Model/Repository/IPostRepository.cs
+++ ExtendedBlog.Model/Repository/IPostRepository.cs
@@ -9,7 +9,7 @@ namespace ExtendedBlog.Model.Repository
 {
     public interface IPostRepository
     {
-        IList<Post> AllPosts();
+        IEnumerable<Post> AllPosts();
         IList<Post> AllPosts(bool onlyPublished);
         int TotalPosts(bool onlyPublished = true);
         int AddPost(Post post);
